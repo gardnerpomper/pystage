683fd00491560aa578a21b651c3ba2e56856fcd5
Gardner Pomper <gardner@networknow.org>
2016-10-09 17:15:56 -0400

    some comments

--- demo.py
+++ demo.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python2.7
+"""Example of using the Stage classes for parallel pipelined processing.
+
+This example has 3 stages.
+
+Stage 1 reads from a directory to get a list of tar.gz files. It writes those
+filenames to an output queue.
+
+Stage 2 reads the names of the tar.gz files from that queue, and for
+each one, it extracts the list of filenames from it. It writes these
+filenames to the next output queue. In this example, we run 3 of these
+threads in parallel.
+
+Stage 3 reads the names of the tar member files and pulls off the file
+extension.  It uses this to build a count of how many files of each
+file extension exist. This example runs 2 of these threads. When all
+the member filenames have been read, the object writes the count
+dictionary to another tar.gz file. Since 2 threads are running, we end
+up with 2 output files, info_5.tgz and info_6.tgz.
+
+All total, this example will run 6 simultaneous threads and exit only when
+they are all finished.
+
+"""
 import os
 import sys
 import os.path
@@ -11,6 +34,12 @@ from collections import Counter
 from stage import Stage
 
 class ListDir(object):
+    """
+    The first stage does not read from and input queue. Instead it reads
+    from the directory given in the class member "dirname". Because it does
+    not read from a queue, or have any cleanup processing, only a __init__
+    method is required.
+    """
     dirname = None
     def __init__(self,envD,outQs):
         print 'ListDir.__init__({})'.format(ListDir.dirname)
@@ -22,6 +51,11 @@ class ListDir(object):
             print 'ListDir.put({})'.format(path)
 
 class ListTar(object):
+    """
+    ListTar receives the name of a tar.gz file from a queue and lists
+    out the names of its members. It writes these names to the output
+    queue in the "per_item()" method.
+    """
     def __init__(self,envD,outQs):
         self._outQs = outQs
 
@@ -35,26 +69,24 @@ class ListTar(object):
         print '<-- ListTar.per_item()'
 
 class Categorize(object):
-    tgz_fname = None
+    """
+    Categorize gets the member filename and splits off the file extension
+    and counts how many of each it receives. When it is done, it creates
+    a new file containing the count dictionary in json format.
+    """
+    count_fname = None
     def __init__(self,envD,outQs):
         self.catD = Counter()
-        self.tgz_fname = Categorize.tgz_fname.format(**envD)
-        print 'Categorize.__init__(tgz_fname={})'.format(self.tgz_fname)
+        self.count_fname = Categorize.count_fname.format(**envD)
+        print 'Categorize.__init__(count_fname={})'.format(self.count_fname)
 
     def per_item(self,q_item):
         base,ext = os.path.splitext(q_item)
         self.catD[ext] += 1
 
     def finish(self):
-        tgz = tarfile.open(self.tgz_fname,'w:gz')
-        tarinfo = tarfile.TarInfo('test')
-        jsonS = json.dumps(self.catD)
-        stringfp = StringIO.StringIO()
-        stringfp.write(jsonS)
-        stringfp.seek(0)
-        tarinfo.size = len(stringfp.buf)
-        tgz.addfile(tarinfo=tarinfo,fileobj=stringfp)
-        tgz.close()
+        with open(self.count_fname,'w') as fp:
+            json.dump(self.catD, fp, sort_keys=True, indent=4)
 
 if __name__ == '__main__':
     #
@@ -66,7 +98,7 @@ if __name__ == '__main__':
     # -------------       -------------       ----------------
     #
     ListDir.dirname = '/Users/gardner/Work/IBM/IBM_CODE_PRE_JUNE_2007'
-    Categorize.tgz_fname = 'info_{THREADNUM}.tgz'
+    Categorize.count_fname = 'count_{THREADNUM}.json'
 
     stages = Stage.pipe( [ListDir], [ListTar]*3, [Categorize]*2)
     for s in stages:
--- stage.py
+++ stage.py
@@ -5,12 +5,30 @@ import Queue
 class MyThread(threading.Thread):
     def __init__(self,stage_class,
                  inQ=None,outQs=None):
+        """
+        initialize with the input and output queues and the class
+        of object that will execute in this thread
+        """
         threading.Thread.__init__(self)
         self._stage_class = stage_class
         self.inQ = inQ
         self.outQs = outQs
 
     def run(self):
+        """
+        this is called when the thread starts, and the thread continues
+        until this method returns. This creates the object that will run
+        in the thread and calls it for every packet that arrives in the input
+        queue.
+        If there is no input queue, the object is assumed to do all its
+        work in its constructor.
+        When the packet from the queue == 'END', the thread will exit, but
+        it will first push the 'END' back on the *input* queue, in case any
+        other threads are also puling from this queue
+        """
+        #
+        # ----- create the processing object
+        #
         if self._stage_class is None:
             return
         #print 'MyThread.run(): vars()={}'.format(vars(self))
@@ -20,12 +38,11 @@ class MyThread(threading.Thread):
         }
         stageObj = self._stage_class(envD,self.outQs)
         #
-        # ----- if there is no input queu, just run the function
-        # ----- with the runArgs given
-        #
-        #
-        # ----- otherwise, call the function once for each packet
-        # ----- in the input queue (and don't use runArgs)
+        # ----- if there is an input queue
+        # -----    send each packet from the queue to the objects
+        # -----    per_item() method
+        # -----    when the packet == 'END', push it back to the queue
+        # -----    and exit
         #
         if self.inQ is not None:
             while True:
@@ -37,24 +54,38 @@ class MyThread(threading.Thread):
                     break
                 if self.outQs is not None:
                     stageObj.per_item(packet)
-
+        #
+        # ----- before exiting, push the 'END' packet to to output queue(s)
+        #
         if self.outQs is not None:
             for outQ in self.outQs.values():
                 if outQ is not None:
                     outQ.put('END')
             print '{}: sent END'.format(self._Thread__name)
-
+        #
+        # ----- if the object has a finish() method, call it before exiting
+        #
         if hasattr(stageObj,'finish'):
             stageObj.finish()
 
 class Stage(object):
     def __init__(self, stage_cls, inQ=None, outQs=None):
+        """
+        create a stage object, which will create multiple threads,
+        each running an object of the class provided in "stage_cls".
+        This object can received packets from and input queue, send
+        packets to output queue(s) or both
+        """
         self._class = stage_cls
         self._inQ = inQ
         self._outQs = outQs
         self._threads = []
 
     def start(self,nThreads=1):
+        """
+        start multiple instances of the object for this stage, each
+        on its own thread
+        """
         for ii in range(nThreads):
             t = MyThread(stage_class=self._class,
                          inQ=self._inQ,
@@ -64,11 +95,46 @@ class Stage(object):
             t.start()
 
     def wait(self):
+        """
+        wait for all the threads in this stage to complete
+        """
         for t in self._threads:
             t.join()
 
     @classmethod
     def pipe(cls, *args):
+        """
+        convenience method to create a pipeline of parallel stages, each running
+        one or more threads. The stages are in a pipeline, where the first stage
+        writes to a queue that is read by the second stage, which writes to a
+        queue read by the third stage, etc.
+
+        Example call:
+
+        stages = Stage.pipe([cls_one],[cls_two]*10,[cls_three]*2)
+
+        will create 3 stages. The first stage will have a single thread containing
+        an object of class "cls_one". The second stage will have 10 threads, each
+        with an object of class "cls_two" and the third stage will have 2 threads
+        of "cls_three" objects.
+
+        Each class must have the following interface:
+        	__init__(self, envD, outQs)
+        	per_item(self,packet)		(optional)
+	        finish(self)			(optional)
+
+        the init get a dict of "environmen variables" it can access in a
+        threadsave manner. Examples are THREADNAME and THREADNUM.
+        Additionly, it gets a dictionary of output queues it can write to. This
+        can be None, if it is not to write to a queue.
+
+        per_item() is called once per item pulled from the input queue (if any)
+        this method should process that item and write the result to the output
+        queue (or queues)
+
+        if the object needs any final processing before it exits (such as closing
+        files), it should implement a finish() method
+        """
         print '{} stages'.format(len(args))
         #
         # ----- create n-1 queues, where n is the number of
